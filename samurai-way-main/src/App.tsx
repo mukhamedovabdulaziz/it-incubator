42b655beedf13fd3b0b78712e28e95c912eb1acc
Abdulaziz <mukhamedovabdulaziz@gmail.com>
2022-09-29 15:40:02 +0600

    add navigation system

--- samurai-way-main/src/App.tsx
+++ samurai-way-main/src/App.tsx
@@ -3,15 +3,31 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
+import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { News } from './components/News/News';
+import { Settings } from './components/Settings/Settings';
+import { Music } from './components/Music/Music';
 
 function App() {
-    return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <Profile/>
+  return (
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header/>
+        <Navbar/>
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path={"/*"} element={<div>404</div>}/>
+            <Route path={"/profile"} element={<Profile/>}/>
+            <Route path={"/dialogs"} element={<Dialogs/>}/>
+            <Route path={"/news"} element={<News/>}/>
+            <Route path={"/music"} element={<Music/>}/>
+            <Route path={"/settings"} element={<Settings/>}/>
+          </Routes>
         </div>
-    );
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
