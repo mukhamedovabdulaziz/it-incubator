7f3abc2067528ff67cfdd43ed0798737ba2c4990
Abdulaziz <mukhamedovabdulaziz@gmail.com>
2022-09-30 15:37:29 +0600

    update dialogs page

--- samurai-way-main/src/components/Dialogs/Dialogs.tsx
+++ samurai-way-main/src/components/Dialogs/Dialogs.tsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import s from "./Dialogs.module.css"
+import {findAllByDisplayValue} from "@testing-library/react";
+import {NavLink} from "react-router-dom";
+
+type dialogItemsPropsType = {
+  name: string
+  id: number
+}
+
+const DialogItems = (props: dialogItemsPropsType) => {
+  return (
+    <div className={`${s.dialog} ${s.active}`}>
+      <NavLink to={"dialogs/" + props.id}>{props.name}</NavLink>
+    </div>
+  );
+}
 
 export const Dialogs = () => {
   return (
-    <div className={"dialogs"}>
-      <div className={"dialogs-items"}>
-        <div className="item">
-          Nazim
-        </div>
-        <div className="item">
-          Bekzhan
-        </div>
-        <div className="item">
-          Timur
-        </div>
-        <div className="item">
-          Aldiyar
-        </div>
-        <div className="item">
-          Ali
-        </div>
-        <div className="item">
-          Arystan
-        </div>
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <DialogItems name={"Nazim"} id={1}/>
+        <DialogItems name={"Bekzhan"} id={2}/>
+        <DialogItems name={"Timur"} id={3}/>
+        <DialogItems name={"Aldiyar"} id={4}/>
+        <DialogItems name={"Ali"} id={5}/>
+        <DialogItems name={"Arystan"} id={6}/>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>How r u?</div>
+        <div className={s.message}>Yo</div>
       </div>
     </div>
-    <div className={"messages"}></div>
-  );
+);
 };
 
