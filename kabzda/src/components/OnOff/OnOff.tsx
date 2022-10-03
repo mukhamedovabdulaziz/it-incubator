325af620d71d7f51a59ab7b35cd29540a196f84b
Abdulaziz <mukhamedovabdulaziz@gmail.com>
2022-10-03 11:52:29 +0600

    add onoff

---
+++ kabzda/src/components/OnOff/OnOff.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const OnOff = () => {
+  return (
+    <div>
+      <div className={"on"}>On</div>
+      <div className={"off"}>Off</div>
+      <div className={"circle"}></div>
+    </div>
+  );
+};
+
