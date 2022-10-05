d62d2d81c8df9f9404d7d18299efeab78ecf8989
Abdulaziz <mukhamedovabdulaziz@gmail.com>
2022-10-05 16:00:18 +0600

    update onoff

--- kabzda/src/components/OnOff/OnOff.tsx
+++ kabzda/src/components/OnOff/OnOff.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export const OnOff = () => {
+type PropsType = {
+  // on: boolean;
+}
+
+export const OnOff = (props: PropsType) => {
+
+  let [on, setOn] = useState(false);
+
+  const onStyle = {
+    width: "30px",
+    height: "20px",
+    border: "1px solid black",
+    display: "inline-block",
+    padding: "2px",
+    backgroundColor: on ? "green" : "white"
+  }
+  const offStyle = {
+    width: "30px",
+    height: "20px",
+    border: "1px solid black",
+    display: "inline-block",
+    padding: "2px",
+    marginLeft: "5px",
+    backgroundColor: on ? "white" : "red"
+  }
+  const indicatorStyle = {
+    width: "15px",
+    height: "15px",
+    borderRadius: "10px",
+    border: "1px solid black",
+    display: "inline-block",
+    marginLeft: "10px",
+    backgroundColor: on ? "green" : "red"
+  }
+
   return (
     <div>
-      <div className={"on"}>On</div>
-      <div className={"off"}>Off</div>
-      <div className={"circle"}></div>
+      <div style={onStyle} onClick={() => {setOn(true)}}>On</div>
+      <div style={offStyle} onClick={() => {setOn(false)}}>Off</div>
+      <div style={indicatorStyle}></div>
     </div>
   );
 };
