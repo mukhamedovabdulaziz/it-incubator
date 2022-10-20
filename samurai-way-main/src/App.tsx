5804bbd1be4eafb29e64a07718dc52748812e792
Abdulaziz <mukhamedovabdulaziz@gmail.com>
2022-10-20 19:24:30 +0600

    change components

--- samurai-way-main/src/App.tsx
+++ samurai-way-main/src/App.tsx
@@ -8,14 +8,14 @@ import {Route, Routes} from "react-router-dom";
 import {News} from './components/News/News';
 import {Settings} from './components/Settings/Settings';
 import {Music} from './components/Music/Music';
-import {addPost, stateType} from "./redux/state";
+import state, {addPost, stateType} from "./redux/state";
 
-type AppsType = {
-  state: stateType
-  addPost: (addPost:string) => void;
-}
+// type AppsType = {
+//   state: stateType
+//   addPost: (addPost:string) => void;
+// }
 
-function App(props: AppsType) {
+function App() {
 
   return (
       <div className='app-wrapper'>
@@ -23,10 +23,10 @@ function App(props: AppsType) {
         <Navbar/>
         <div className="app-wrapper-content">
           <Routes>
-            <Route path={"/profile"} element={<Profile posts={props.state.profilePage.posts}
+            <Route path={"/profile"} element={<Profile posts={state.profilePage.posts}
                                                        addPost={addPost}/>}/>
-            <Route path={"/dialogs"} element={<Dialogs dialogs={props.state.dialogsPage.dialogs}
-                                                       messages={props.state.dialogsPage.messages}/>}/>
+            <Route path={"/dialogs"} element={<Dialogs dialogs={state.dialogsPage.dialogs}
+                                                       messages={state.dialogsPage.messages}/>}/>
             <Route path={"/news"} element={<News/>}/>
             <Route path={"/music"} element={<Music/>}/>
             <Route path={"/settings"} element={<Settings/>}/>
--- samurai-way-main/src/render.tsx
+++ samurai-way-main/src/render.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost, stateType} from "./redux/state";
+import state, {addPost, stateType} from "./redux/state";
 import {BrowserRouter} from "react-router-dom";
 
 export let rerenderEntireTitle = (state: stateType) => {
   ReactDOM.render(
     <BrowserRouter>
-      <App state={state} addPost={addPost}/>
+      <App />
     </BrowserRouter>, document.getElementById('root'));
 }
