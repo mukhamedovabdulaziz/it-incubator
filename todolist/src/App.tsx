1c47d8bedf65dee7ffec246cf343dbd8ac829ba2
Abdulaziz <52410906+mukhamedovabdulaziz@users.noreply.github.com>
2025-10-30 12:51:32 +0500

    feat: refactor App component to use TaskList and TodolistItem, update import style

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,33 +1,39 @@
 import './App.css'
+import { TaskList, TaskListPropsType } from './TaskList'
+import { TodolistItem } from './TodolistItem'
 
-function App() {
-  return (
-      <div className="app">
-        <div>
-          <h3>What to learn</h3>
-          <div>
-            <input/>
-            <button>+</button>
-          </div>
-          <ul>
-            <li>
-              <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={true}/> <span>JS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={false}/> <span>React</span>
-            </li>
-          </ul>
-          <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-          </div>
-        </div>
-      </div>
-  )
+export type Task = {
+  id: number
+  title: string
+  isDone: boolean
 }
 
-export default App
+export const App = () => {
+  const tasks1: Task[] = [
+    { id: 1, title: 'HTML&CSS', isDone: true },
+    { id: 2, title: 'JS', isDone: true },
+    { id: 3, title: 'ReactJS', isDone: false },
+    { id: 4, title: 'Redux', isDone: false },
+    { id: 5, title: 'Typescript', isDone: false },
+    { id: 6, title: 'RTK query', isDone: false },
+  ]
+
+  const taskList: TaskListPropsType = {
+    data: {
+      title: 'Task List',
+      tasks: [
+        { taskId: 1, title: 'Learn HTML', isDone: true },
+        { taskId: 2, title: 'Learn CSS', isDone: true },
+        { taskId: 3, title: 'Learn JavaScript', isDone: false },
+      ],
+      students: ['Alice', 'Bob', 'Charlie'],
+    }
+  }
+
+  return (
+    <div className="app">
+      <TodolistItem title="What to learn" tasks={tasks1} />
+      <TaskList data={taskList.data} />
+    </div>
+  )
+}
